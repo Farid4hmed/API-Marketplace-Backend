ab6dcc8fb28a895d24b86aff59248b273f12a290
Farid Ahmed <faridahmed.tech@gmail.com>
2023-01-02 14:34:35 +0530

    added database config and initialzation

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const initDB = require("./config/db.js");
 const express = require("express");
 const app = express();
 
+initDB();
 
+app.get("/api/health", (req, res) => {
+    res.send({
+        time: new Date(),
+        server: "Shuffle Backend",
+        status: "Active",
+    });
+});
 
 
 
@@ -12,3 +24,4 @@ const host = process.env.HOST || "localhost";
 app.listen(port, function(req, res){
     console.log(`Express server is up and running at http://${host}:${port}`);
 });
+
